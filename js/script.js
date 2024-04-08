5e55bd4cd87434d38d07817746e989a68b8efe77
Wmduggan41 <wmduggan@gmail.com>
2024-04-08 16:41:38 -0400

    working slider

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 document.getElementById('slider-range').addEventListener('input', function(e) {
-  const sliderValue = e.target.value;
-  const overlayWidth = 100 - sliderValue;
-  const overlay = document.querySelector('.media-slider__left');
-  overlay.style.width = `${overlayWidth}%`;
+    const sliderValue = e.target.value;
+    const overlay = document.querySelector('.media-slider__left');
+    overlay.style.width = `${sliderValue}%`;
+    const splitter = document.querySelector('.media-slider__splitter');
+    splitter.style.left = `${sliderValue}%`;
 });
+
+
 
 
